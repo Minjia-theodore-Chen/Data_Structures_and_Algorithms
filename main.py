38a9a70d72f5311363e30ae6dfcb45d25857c9f2
Minjia-theodore-Chen <minjia.theodore.chen@gmail.com>
2021-11-07 19:14:45 +0100

    init

--- main.py
+++ main.py
@@ -7,25 +7,10 @@ def clear():
     else:
         _ = system('clear')
 
-class signal:
-    def __init__(self, frequency, amplitude) -> None:
-        self.frequency = frequency
-        self.amplitude = amplitude
-        self.phase_0 = None
-
-    def phase(self, time, phase_0):
-        if phase_0 != None:
-            self.phase_0 = phase_0
-            return self.phase_0 + 2 * time * self.frequency * math.pi
-        else:
-            print('Specify initial phase!')
-            return None
-
 def main():
     clear()
-    Ntz_de = signal(50, 230)
-    Phase = Ntz_de.phase(1, 0)
-    print(Phase)
+    student = {'name' : 'Minjia','age' : 25, 'course' : ['Math', 'CompSci']}
+    print(student['course'])
 
 if __name__ =='__main__':
     main()
